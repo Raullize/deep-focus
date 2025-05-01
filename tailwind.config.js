dc9c270b9f5e5438c969597132e2920fa4145865
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-05-01 20:52:45 -0300

    ✨ feat: adiciona arquivo tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#121212',
+        primary: '#BB86FC',
+        secondary: '#03DAC6',
+        error: '#CF6679',
+        onBackground: '#E1E1E1',
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+} 
